fd23050344937366618cf4f9ac46ff3fe3ab5029
maria eduarda <duda.guadagno.meg@gmail.com>
2023-06-07 17:52:37 -0300

    feat: mudanca no css, no index e melhora no scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
-const peso = document.getElementById("peso").value;
-const altura = document.querySelector("altura").value;
+function calcular(event) {
+    event.preventDefault();
+    const inputPeso = document.getElementById("peso");
+    const inputAltura = document.getElementById("altura");
+    const peso = inputPeso.value;
+    const altura = inputAltura.value;
+    
+    if (peso > 300 || peso < 1) {
+        inputPeso.style.border = "1px solid red";
+    } else {
+        inputPeso.style.border = "1px solid white";
+    }
+
+    if (altura > 300 || peso < 20) {
+        inputAltura.style.border = "1px solid red";
+    } else {
+        inputAltura.style.border = "1px solid white";
+    }
+}
