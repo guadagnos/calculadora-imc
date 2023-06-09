389883ab61a55a02e5223f215df48bad68ab2845
maria eduarda <duda.guadagno.meg@gmail.com>
2023-06-09 15:36:23 -0300

    feat: verificar limites para calcular

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,7 @@ function calcular(event) {
     const inputPeso = document.getElementById("peso");
     const inputAltura = document.getElementById("altura");
     const peso = inputPeso.value;
-    const altura = inputAltura.value/100;
-    const imc = peso / (altura*altura);    
+    const altura = inputAltura.value/100;    
     
     if (peso > 300 || peso < 1) {
         inputPeso.style.border = "1px solid red";
@@ -12,10 +11,13 @@ function calcular(event) {
         inputPeso.style.border = "1px solid white";
     }
 
-    if (altura > 300 || peso < 20) {
+    if (altura > 300 || altura < 20) {
         inputAltura.style.border = "1px solid red";
     } else {
         inputAltura.style.border = "1px solid white";
     }
 
+    if (peso < 300 || peso > 1 && altura < 300 || altura > 20) {
+        const imc = peso / (altura*altura);
+    }    
 }
