a5f91dcd0faa5c8ecffdd979405005dbcc9631df
maria eduarda <duda.guadagno.meg@gmail.com>
2023-06-12 10:56:45 -0300

    feat: alterei o limite de cm para m

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,14 @@ function calcular(event) {
         inputPeso.style.border = "1px solid white";
     }
 
-    if (altura > 300 || altura < 20) {
+    if (altura > 3 || altura < 0.2) {
         inputAltura.style.border = "1px solid red";
     } else {
         inputAltura.style.border = "1px solid white";
     }
 
-    if (peso < 300 || peso > 1 && altura < 300 || altura > 20) {
+    if (peso < 300 || peso > 1 && altura < 3 || altura > 0.2) {
         const imc = peso / (altura*altura);
+        console.log(imc);
     }    
 }
