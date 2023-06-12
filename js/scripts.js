b8f440bff3bccda7981c34974d22da24e7a48346
maria eduarda <duda.guadagno.meg@gmail.com>
2023-06-12 15:05:34 -0300

    feat: classificação do imc

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@ function calcular(event) {
     const inputPeso = document.getElementById("peso");
     const inputAltura = document.getElementById("altura");
     const peso = inputPeso.value;
-    const altura = inputAltura.value/100;    
+    const altura = inputAltura.value/100;
+    const resultadoElement = document.getElementById("resultado");    
     
     if (peso > 300 || peso < 1) {
         inputPeso.style.border = "1px solid red";
@@ -19,8 +20,20 @@ function calcular(event) {
 
     if (peso < 300 && peso > 1 && altura < 3 && altura > 0.2) {
         const imc = peso / (altura*altura);
-        const imcArredondado = imc.toFixed(2);
-        console.log(imcArredondado);
-        
-    }    
+        const imcArredondado = imc.toFixed(2);        
+        var resultado=""
+        if (imcArredondado < 18.5) {
+            resultado= `IMC: ${imcArredondado} \n Abaixo do peso`;
+        }
+        if (imcArredondado >= 18.5 && imcArredondado <= 24.99) {
+            resultado= `IMC: ${imcArredondado} \n Peso ideal`;
+        }
+        if (imcArredondado >= 25 && imcArredondado <= 29.99) {
+            resultado= `IMC: ${imcArredondado} \n Sobrepeso`;
+        }
+        if (imcArredondado > 30) {
+            resultado= `IMC: ${imcArredondado} \n Obesidade`;
+        }
+    }   
+    resultadoElement.innerText = resultado;    
 }
